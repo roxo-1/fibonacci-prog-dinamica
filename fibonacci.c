df731d4b0a408779847912c1a5312a36997e1ed2
Carolina Lee <111212023+roxo-1@users.noreply.github.com>
2025-08-27 11:17:59 +0000

    COMMIT "contadores de chamadas" UPDATE "rename fibonacciDinamico'

--- fibonacci.c
+++ fibonacci.c
@@ -1,16 +1,19 @@
 #include <stdio.h>
+long long chamadas = 0;
 
 int F(int n)
 {
+    chamadas++;
 	if(n<1) return 0;
 	else if(n==1) return 1;
 	else return F(n-1) + F(n-2);
 }
 
 int main() {
-    int num = 10;
+    int num = 40;
     int resultado = F(num);
     printf("O resultado é: %d\n", resultado);
+    printf("Número de chamadas da função: %lld\n", chamadas);
 
     return 0;
 }
---
+++ fibonacciDinamico.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+// Definimos o tamanho do array para armazenar os valores
+#define MAX 100
+
+// Contador global para rastrear as chamadas da função
+long long chamadas_memo = 0;
+int memo[MAX];
+
+int F_memo(int n) {
+    chamadas_memo++; // O contador é incrementado a cada chamada
+    
+    if (n <= 1) {
+        return n;
+    }
+    
+    // Se o valor já foi calculado, retorna ele
+    if (memo[n] != 0) {
+        return memo[n];
+    }
+    
+    // Calcula o valor, armazena no array e retorna
+    memo[n] = F_memo(n - 1) + F_memo(n - 2);
+    
+    return memo[n];
+}
+
+int main() {
+    int num = 40;
+    
+    // Inicializa o array com zeros
+    for (int i = 0; i < MAX; i++) {
+        memo[i] = 0;
+    }
+    
+    int resultado = F_memo(num);
+    
+    printf("O 40º termo de Fibonacci é: %d\n", resultado);
+    printf("Número de chamadas da função: %lld\n", chamadas_memo);
+
+    return 0;
+}
